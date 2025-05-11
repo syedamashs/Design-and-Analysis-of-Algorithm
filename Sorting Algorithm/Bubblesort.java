6f2e16f9e8d97b8c59bd2b03ba1da13a0b7b870e
SYED AMASH S <syedamashtce@gmail.com>
2025-05-11 23:19:07 +0530

    Update Bubblesort.java

--- Sorting Algorithm/Bubblesort.java
+++ Sorting Algorithm/Bubblesort.java
@@ -1,1 +1,18 @@
+//Just an pseudoCode or Algorithm!!!!
 
+for i in range(0,n-1):
+  int flag=0;
+  for j in range(i,n-1):
+    if(arr[j+1]>arr[j]){
+      temp=arr[j];
+      arr[j]=arr[j+1];
+      arr[j+1]=arr[j];
+      flag=1;
+    }
+
+ if flag==0:
+    break;
+
+
+  
+  
