c81150db357eb5b627f01a6bea2498c97122119b
SYED AMASH S <syedamashtce@gmail.com>
2025-05-12 22:53:15 +0530

    Update selectionsort.java

--- Sorting Algorithm/selectionsort.java
+++ Sorting Algorithm/selectionsort.java
@@ -1,1 +1,14 @@
+//Just an Algorithm!!!
 
+for(int i=0;i<n-1;i++){
+  int min = i;
+
+  for(int j=i+1;j<n;j++){
+    if(arr[j]<arr[min]){
+      j=min;
+    }
+  }
+if(min!=i){
+  swap(arr[i],arr[min]);
+}
+}
