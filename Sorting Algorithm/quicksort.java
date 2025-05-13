92dbffa57b332bab6550d396e16ace78588166a9
SYED AMASH S <syedamashtce@gmail.com>
2025-05-13 16:04:00 +0530

    Update quicksort.java

--- Sorting Algorithm/quicksort.java
+++ Sorting Algorithm/quicksort.java
@@ -1,1 +1,36 @@
+//partition code for Quick Sort !!
+partition(arr,lb,ub){
 
+int pivot = arr[lb];
+int start = lb;
+int end = ub;
+
+while(start < end){
+  while(pivot >= arr[start]){
+    start++;
+  }
+  while(pivot < arr[end]){
+    end--;
+  }
+
+  if(start<end){
+    swap(arr[start],arr[end]);
+  }
+
+}
+
+swap(arr[end],pivot);
+return end;
+
+
+//Code for Base of QUICK SORT !!!
+
+quicksort(arr,lb,ub){
+  if(lb<ub){
+    loc=partition(arr,lb,ub);
+    quicksort(arr,lb,loc);
+    quicksort(arr,loc+1,ub);
+
+  }
+}
+  
