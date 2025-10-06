f199782db1aac2c4c315936e7957a849e10c3148
SYED AMASH S <syedamashtce@gmail.com>
2025-10-06 23:55:53 +0530

    Update Insertionsort.java

--- Sorting Algorithm/Insertionsort.java
+++ Sorting Algorithm/Insertionsort.java
@@ -1,8 +1,19 @@
-for i in range(1,n):
-  temp=arr[i];
-  j=i-1;
-  while(j>=0 && arr[j]>arr[i]){
-    arr[j+1]=arr[j];
-    j--;
-  }
-  arr[j+1]=temp;
+class Solution {
+    // Please change the array in-place
+    public void insertionSort(int arr[]) {
+        // code here
+        
+        for(int i=1;i<arr.length;i++){
+            int temp = arr[i];
+            int j = i-1;
+            
+            while(j>=0 && arr[j]>temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            
+            arr[j+1] = temp;
+        }
+        
+    }
+}
